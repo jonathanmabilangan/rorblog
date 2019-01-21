b8bf39eccb7c92e34c1b497c7018b5166b6b9867
Jonathan Mabilangan <jonathanmabilangan@Altuss-MacBook-Pro.local>
2019-01-21 11:58:10 +0800

    Added bootstrap

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,20 +3,20 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: "pong", password: "secret", only: :destroy
 
   def create
-    @artice = Article.find(params[:article_id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to artice_path(@article)
+    redirect_to article_path(@article)
   end
-
+ 
   def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to artice_path(@article)
+    redirect_to article_path(@article)
   end
-
+ 
   private
     def comment_params
-      params.require(:comment).permit(:commented, :body)
+      params.require(:comment).permit(:commenter, :body)
     end
 end
